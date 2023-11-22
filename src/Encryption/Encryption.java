a6bc0b6db9b19b9cb95e5ff67664f1f6423c338a
RahmaHassanAli <rahmmahassan388@gmail.com>
2023-11-22 19:14:00 +0200

    Adding two packages (EmailNotification,Encryption) which contain skeletons for (emailNotifier,Encryption) classes.

---
+++ src/Encryption/Encryption.java
@@ -0,0 +1,11 @@
+package Encryption;
+
+public class Encryption {
+    public String encryptPassword(String password){
+        return null;
+    }
+    public String decryptPassword(String password){
+        return null;
+    }
+
+}
