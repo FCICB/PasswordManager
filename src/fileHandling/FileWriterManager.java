3421caec23912469469e3872472bfc7e039fe4b2
Shaimaa-Sabry1 <104500613+Shaimaa-Sabry1@users.noreply.github.com>
2023-11-25 16:26:03 +0200

    Update FileWriterManager.java
    remove comment

--- src/fileHandling/FileWriterManager.java
+++ src/fileHandling/FileWriterManager.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class FileWriterManager
 {
     WriterFactory writerFactory;
-    public boolean exportVault(String path, String type)//missing parameter list of password vaults.
+    public boolean exportVault(String path, String type)
     {
         return false;
     }
