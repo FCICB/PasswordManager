0bc1a8ce4704ccaef87e353bf5673e5a9e3368ac
Ziad Ahmed <zyadahmed2466@gmail.com>
2023-12-15 02:24:39 +0200

    add database connection implementaion and add conf file for database variables

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -1,10 +1,20 @@
 package Database;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DatabaseConnection {
-    private static final String DB_URL = " ";
-    private static final String DB_USER=  " " ;
-    private static final String DB_PASSWORD = " ";
+    private static  String DB_URL ;
+    private static  String DB_USER ;
+    private static  String DB_PASSWORD ;
     private volatile static  DatabaseConnection instance ;
+    private  static final String confPath = "src/config.properties";
+    private static Connection DBConnectionInstance;
 
 
     public static  DatabaseConnection getInstance() {
@@ -19,13 +29,32 @@ public class DatabaseConnection {
     }
 
 
-    public void closeConnection (){
-        //close the connection f the database
+    public void closeConnection () throws SQLException {
+        if (getDBConnectionInstance() != null){
+            getDBConnectionInstance().close();
+        }
 
+    }
+    public Connection getDBConnectionInstance(){
+        return DBConnectionInstance;
     }
 
     private DatabaseConnection (){
-        // Initialize the database connection
+        try(FileInputStream confInput = new FileInputStream(confPath);) {
+            Properties conf = new Properties();
+            conf.load(confInput);
+            DB_URL = conf.getProperty("DB_URL");
+            DB_USER = conf.getProperty("DB_USER");
+            DB_PASSWORD = conf.getProperty("DB_PASSWORD");
+            DBConnectionInstance = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
